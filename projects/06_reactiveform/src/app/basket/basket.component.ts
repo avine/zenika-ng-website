c1fdb2bf17ebf0aee2fac582af2bc273c8120374
Stéphane Francel <stephane.francel@gmail.com>
2023-05-04 23:52:22 +0200

    06_reactiveform (copy/paste from previous)

---
+++ projects/06_reactiveform/src/app/basket/basket.component.ts
@@ -0,0 +1,39 @@
+import { AsyncPipe, CurrencyPipe, NgFor, NgIf } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AlertService } from '../alert/alert.service';
+import { Customer } from '../customer/customer.types';
+import { BasketService } from './basket.service';
+
+@Component({
+  selector: 'app-basket',
+  standalone: true,
+  imports: [AsyncPipe, CurrencyPipe, NgFor, NgIf],
+  templateUrl: './basket.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class BasketComponent {
+  protected customer: Customer = { name: '', address: '', creditCard: '' };
+
+  protected basketService = inject(BasketService);
+
+  #alertService = inject(AlertService);
+
+  #router = inject(Router);
+
+  protected checkout(event: Event): void {
+    event.stopPropagation();
+    event.preventDefault();
+
+    this.basketService.checkout(this.customer).subscribe({
+      next: ({ orderNumber }) => {
+        this.#alertService.addSuccess(`🚀 Merci pour votre commande (réf. ${orderNumber}).`);
+        this.#router.navigate(['']);
+      },
+      error: () => {
+        this.#alertService.addDanger("😱 Désolé, une erreur s'est produite.");
+      },
+    });
+  }
+}
